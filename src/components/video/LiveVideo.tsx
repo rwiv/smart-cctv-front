a49ee57122866a74eb4d4709221db2803cce24d1
rwiv <zc798546@gmail.com>
2024-05-27 12:59:19 +0900

    feat: add LiveVideo features

--- src/components/video/LiveVideo.tsx
+++ src/components/video/LiveVideo.tsx
@@ -1,30 +1,66 @@
-import {useEffect, useRef} from "react";
-import Hls from "hls.js";
+import {useEffect, useRef, useState} from "react";
+import Hls, {Level} from "hls.js";
+import {Button} from "@/components/ui/button.tsx";
+
+const liveUrl1 = "http://localhost:8081/hls/hello.m3u8";
 
 export function LiveVideo() {
 
-  const ref = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const hlsRef = useRef<Hls>(new Hls());
+  const [levels, setLevels] = useState<Level[]>([]);
+  const [curLevel, setCurLevel] = useState<Level>();
 
   useEffect(() => {
+    console.log(levels);
+  }, [levels]);
+
+  useEffect(() => {
+    initHls();
+
+    videoRef.current?.addEventListener("play", () => {
+      // 재개 시 최신 segment를 새롭게 load
+      if (videoRef.current?.currentTime ?? -1 > 0) {
+        initHls();
+      }
+    });
+
+    videoRef.current?.addEventListener("pause", () => {
+      // 일시 정지 시 segment loading stop
+      hlsRef.current.stopLoad();
+    });
+
+    window.addEventListener("focus", () => {
+      resetHls();
+    })
+
+    window.addEventListener("blur", () => {
+      hlsRef.current.stopLoad();
+    })
+  }, []);
+
+  const onSelectLevel = (levelIdx: number) => {
+    hlsRef.current.currentLevel = levelIdx;
+  }
+
+  const initHls = () => {
     if (!Hls.isSupported()) {
       console.log("not supported");
       return;
     }
 
     const hls = new Hls({
-      // startLevel을 -1로 설정해야 최초 play 시 bandwidth에 맞는 화질을 채택함
-      startLevel: -1,
-      // liveDurationInfinity: true
+      startLevel: -1, // startLevel을 -1로 설정해야 최초 play 시 bandwidth에 맞는 화질을 채택함
+      liveSyncDurationCount: 1, // 앞에 1개의 segment 여분을 남겨두고 시작. 즉, 3번째 hls segment부터 시작
     });
-    // const liveUrl1 = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8";
-    const liveUrl1 = "http://localhost:8081/hls/hello.m3u8";
-    // const m3u8Url = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+    hlsRef.current = hls;
     hls.loadSource(liveUrl1);
-    hls.attachMedia(ref.current as any);
+    hls.attachMedia(videoRef.current as any);
     hls.on(Hls.Events.MANIFEST_PARSED, async () => {
-      console.log("play");
-      const video = ref.current;
+      setLevels(hls.levels)
+      const video = videoRef.current;
       if (video === null) return;
+
       // refer: https://developer.chrome.com/blog/autoplay?hl=ko#webaudio
       // 유저의 미디어 참여 지수가 임계값을 넘으면(이전에 재생한 적이 있으면) 자동실행된다
       // 그렇지 않아 자동실행에 실패했다면 mute 후 자동실행하면 된다
@@ -34,12 +70,40 @@ export function LiveVideo() {
         video.muted = true;
         await video.play();
       }
+      console.log("play");
     });
-  }, []);
+
+    hls.on(Hls.Events.LEVEL_LOADED, async (_, data) => {
+      setCurLevel(hls.levels[data.level]);
+    });
+  }
+
+  const resetHls = () => {
+    hlsRef.current.destroy();
+    initHls();
+  }
+
+  const onTest = () => {
+    console.log(levels)
+    console.log(hlsRef.current.loadLevel)
+  }
 
   return (
     <div>
-      <video ref={ref} controls />
+      <video ref={videoRef} css={{width: 1280, height: 720}} controls />
+      <div>
+        {curLevel && (
+          <div>current quality: {curLevel?.height + "p"}</div>
+        )}
+        {levels.map((level, idx) => (
+          <span key={idx}>
+            <Button onClick={() => onSelectLevel(idx)}>{level.height}p</Button>
+          </span>
+        ))}
+        <div>
+          <Button onClick={onTest}>test</Button>
+        </div>
+      </div>
     </div>
   )
 }
